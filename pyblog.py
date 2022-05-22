5a808b85cd50f9d9250980c5ca8a566ddda75b6b
jay <jayprickett@gmail.com>
2022-05-22 11:39:01 -0500

    date parsing function and time functionality added

--- pyblog.py
+++ pyblog.py
@@ -14,13 +14,13 @@ files = list()
 tags = set()
 authors = set()
 posts = dict()
-years = ()
+years = set()
 
 
 def main():
     make_list_of_files()
     read_markdown_fill_posts(files)
-    # read_markdown_write_individual_html(files)
+    read_markdown_write_individual_html(files)
     read_markdown_create_indices(posts)
     return
 
@@ -32,6 +32,50 @@ def parse_json_config(config_file, request_string):
         return data_return
 
 
+def parse_date_time(auto_date, manual_date):
+    date_format = parse_json_config(config, "date_format")
+    manual_formatted_date = manual_date
+    twenty_four_hour_time_format = parse_json_config(config, "twenty_four_hour_time_format")
+    year_position = date_format.find("Y")
+    month_position = date_format.find("M")
+    day_position = date_format.find("D")
+    hour_position = date_format.find("H")
+    minute_position = date_format.find("m")
+    period_position = date_format.find("[")
+    
+    if auto_date == None:
+        date_period = manual_date[period_position:period_position+2]
+        date_year = manual_date[year_position:year_position+4]
+        date_day = manual_date[day_position:day_position+2]
+        date_month = manual_date[month_position:month_position+2]
+        if twenty_four_hour_time_format == False:
+            if date_period == "PM":
+                date_hour = str(int(manual_date[hour_position:hour_position+2])+12)
+        else:
+            date_period = ""
+        date_hour = manual_date[hour_position:hour_position+2]
+        date_minute = manual_date[minute_position:minute_position+2]
+    else:
+        date_year = str(auto_date.year)
+        date_day = str(auto_date.day).zfill(2)
+        date_month = str(auto_date.month).zfill(2)
+        date_hour = str(auto_date.hour).zfill(2)
+        date_minute = str(auto_date.minute).zfill(2)
+        if twenty_four_hour_time_format == False:
+            if int(date_hour) > 12:
+                date_period = "PM"
+                date_hour = str(int(date_hour)-12).zfill(2)
+            else:
+                date_period = "AM"
+        else:
+            date_period = ""
+        
+        manual_formatted_date = date_format.replace("YYYY", date_year).replace("MM", date_month).replace("DD", date_day).replace("HH", date_hour).replace("mm", date_minute).replace("[AM/PM]", date_period)
+    
+    sortable_date = str(date_year) + str(date_month) + str(date_day) + str(date_hour) + str(date_minute)
+
+    return sortable_date, manual_formatted_date, date_year, date_month, date_day, date_hour, date_minute, date_period
+
 
 def make_list_of_files():
     for subdir, dirs, files_names in os.walk(parse_json_config(config, "markdown_directory")):
@@ -39,8 +83,8 @@ def make_list_of_files():
             files.append(os.path.join(subdir, file))
 
 
-def read_markdown_write_individual_html(files):
-    for file in files:
+def read_markdown_write_individual_html(list_of_files):
+    for file in list_of_files:
         file_name = os.path.basename(file).replace(".md", "").replace(".txt", "").strip().lower()
         with open(file, "r", encoding="utf-8") as md_file:
             md_text = md_file.read()
@@ -55,12 +99,16 @@ def read_markdown_write_individual_html(files):
             footer = footer.read()
             footer = footer.replace("[[$CONTENT]]", markdown.markdown(footer_md))
             html = html + markdown.markdown(footer)
-        with open(parse_json_config(config, "html_directory") + str(posts.get(file_name)[3]) + "/" + file_name + ".html", "w", encoding="utf-8", errors="xmlcharrefreplace") as html_file:
-            html_file.write(html)
+        if parse_json_config(config, "add_sortable_date_to_file_name") == False:
+            with open(parse_json_config(config, "html_directory") + str(posts.get(file_name)[3]) + "/" + file_name + ".html", "w", encoding="utf-8", errors="xmlcharrefreplace") as html_file:
+                html_file.write(html)
+        else: 
+            with open(parse_json_config(config, "html_directory") + str(posts.get(file_name)[3]) + "/" + str(posts.get(file_name)[0]) + "-" + file_name + ".html", "w", encoding="utf-8", errors="xmlcharrefreplace") as html_file:
+                html_file.write(html)
 
 
-def read_markdown_fill_posts(files):
-    for file in files:
+def read_markdown_fill_posts(list_of_files):
+    for file in list_of_files:
         file_title = ""
         file_date = ""
         file_year = ""
@@ -76,6 +124,7 @@ def read_markdown_fill_posts(files):
                     file_title = line.replace("Title: ", "").strip()
                 elif "Date: " in line:
                     file_date = line.replace("Date: ", "").strip()
+                    file_year = parse_date_time(None, file_date)[2]
                 elif "Author: " in line:
                     file_author = line.replace("Author: ", "").strip()
                     authors.add(file_author)
@@ -90,21 +139,16 @@ def read_markdown_fill_posts(files):
                         file_title = file_name
                     if file_date == "":
                         file_cdate = datetime.strptime(time.ctime(os.path.getmtime((file))), "%a %b %d %H:%M:%S %Y")
-                        file_date = parse_json_config(config, "date_format").replace("YYYY", str(file_cdate.year)).replace("MM", str(file_cdate.month).zfill(2)).replace("DD", str(file_cdate.day).zfill(2))
+                        file_date = parse_date_time(file_cdate,file_cdate)
+                        file_date = file_date[1]
                         file_year = str(file_cdate.year)
                     if file_author == "":
                         file_author = parse_json_config(config, "default_author")
 
                 years.add(file_year)
-                year_position = parse_json_config(config, "date_format").find("Y")
-                month_position = parse_json_config(config, "date_format").find("M")
-                day_position = parse_json_config(config, "date_format").find("D")
-                file_year = file_date[year_position:year_position+4]
-                file_day = file_date[day_position:day_position+2]
-                file_month = file_date[month_position:month_position+2]
-                file_date_sortable = file_year + file_month + file_day
+                sortable_date = parse_date_time(None, file_date)[0]
 
-            posts.update({file_name: (file_date_sortable, file_title, file_date, file_year, file_author, file_summary, file_tags)}) 
+            posts.update({file_name: (sortable_date, file_title, file_date, file_year, file_author, file_summary, file_tags)}) 
  
 
 def read_markdown_create_indices(posts):
--- to_do.md
+++ to_do.md
@@ -1,6 +1,6 @@
 # Still To Do  
 
 * finish indicies function
-* rewrite date formatting and functionality to include time
+* ~~rewrite date formatting and functionality to include time~~
 * fix html issues with indices
-* rewrite function for creating individual html files, need to change the header rows to be more like the index file
+* rewrite function for creating individual html files, need to change the header rows to be more like the index file. probably need to fix indices first
