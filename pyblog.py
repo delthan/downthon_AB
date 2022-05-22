3cf4ff1cccd6b484fe7befb58180dcdee98ac737
jay <jayprickett@gmail.com>
2022-05-22 09:59:49 -0500

    starting to rebuild date parsing

--- pyblog.py
+++ pyblog.py
@@ -6,39 +6,37 @@ import json
 from pathlib import Path
 
 
-files = list()
 header_html = str("./config/header.html")
 footer_html = str("./config/footer.html")
 index_html = str("./config/index.html")
 config = str("./config/config.json")
+files = list()
 tags = set()
 authors = set()
 years = set()
 posts = dict()
 
+
 def main():
-    parse_config_json(config)
     make_list_of_files()
     read_markdown_fill_posts(files)
     # read_markdown_write_individual_html(files)
     read_markdown_create_indices(posts)
     return
 
-def parse_config_json(config_file):
+
+def parse_json_config(config_file, request_string):
     with open(config_file) as f:
         data = json.load(f)
-        parse_config_json.date_format = data.get("date_format")
-        parse_config_json.html_directory = data.get("html_directory")
-        parse_config_json.markdown_directory = data.get("markdown_directory")
-        parse_config_json.add_sortable_date_to_file_name = data.get("add_sortable_date_to_file_name")
-        parse_config_json.header_markdown = data.get("header_markdown")
-        parse_config_json.footer_markdown = data.get("footer_markdown")
-        parse_config_json.swag_markdown = data.get("swag_markdown")
+        data_return = data.get(request_string)
+        return data_return
+
 
 def make_list_of_files():
-    for subdir, dirs, files_names in os.walk(parse_config_json.markdown_directory):
+    for subdir, dirs, files_names in os.walk(parse_json_config(config, "markdown_directory")):
         for file in files_names:
             files.append(os.path.join(subdir, file))
+
 
 def read_markdown_write_individual_html(files):
     for file in files:
@@ -46,18 +44,19 @@ def read_markdown_write_individual_html(files):
         with open(file, "r", encoding="utf-8") as md_file:
             md_text = md_file.read()
             html = markdown.markdown(md_text)
-        with open(parse_config_json.header_markdown) as header_md, open(header_html) as header:
+        with open(parse_json_config(config, "header_markdown")) as header_md, open(header_html) as header:
             header = header.read()
             header_md = header_md.read()
             header = header.replace("[[$CONTENT]]", markdown.markdown(header_md))
             html = header + html
-        with open(parse_config_json.footer_markdown) as footer_md, open(footer_html) as footer:
+        with open(parse_json_config(config, "footer_markdown")) as footer_md, open(footer_html) as footer:
             footer_md = footer_md.read()
             footer = footer.read()
             footer = footer.replace("[[$CONTENT]]", markdown.markdown(footer_md))
             html = html + markdown.markdown(footer)
-        with open(parse_config_json.html_directory + str(posts.get(file_name)[3]) + "/" + file_name + ".html", "w", encoding="utf-8", errors="xmlcharrefreplace") as html_file:
+        with open(parse_json_config(config, "html_directory") + str(posts.get(file_name)[3]) + "/" + file_name + ".html", "w", encoding="utf-8", errors="xmlcharrefreplace") as html_file:
             html_file.write(html)
+
 
 def read_markdown_fill_posts(files):
     for file in files:
@@ -90,15 +89,15 @@ def read_markdown_fill_posts(files):
                         file_title = file_name
                     if file_date == "":
                         file_cdate = datetime.strptime(time.ctime(os.path.getmtime((file))), "%a %b %d %H:%M:%S %Y")
-                        file_date = parse_config_json.date_format.replace("YYYY", str(file_cdate.year)).replace("MM", str(file_cdate.month).zfill(2)).replace("DD", str(file_cdate.day).zfill(2))
+                        file_date = parse_json_config(config, "date_format").replace("YYYY", str(file_cdate.year)).replace("MM", str(file_cdate.month).zfill(2)).replace("DD", str(file_cdate.day).zfill(2))
                         file_year = str(file_cdate.year)
                     if file_author == "":
-                        file_author = "anonymous"
+                        file_author = parse_json_config(config, "default_author")
 
                 years.add(file_year)
-                year_position = parse_config_json.date_format.find("Y")
-                month_position = parse_config_json.date_format.find("M")
-                day_position = parse_config_json.date_format.find("D")
+                year_position = parse_json_config(config, "date_format").find("Y")
+                month_position = parse_json_config(config, "date_format").find("M")
+                day_position = parse_json_config(config, "date_format").find("D")
                 file_year = file_date[year_position:year_position+4]
                 file_day = file_date[day_position:day_position+2]
                 file_month = file_date[month_position:month_position+2]
@@ -106,6 +105,7 @@ def read_markdown_fill_posts(files):
 
             posts.update({file_name: (file_date_sortable, file_title, file_date, file_year, file_author, file_summary, file_tags)}) 
  
+
 def read_markdown_create_indices(posts):
     index_html_output = ""
     authors_html = ""
@@ -115,7 +115,7 @@ def read_markdown_create_indices(posts):
         post_value = str(posts.get(post)[0])
         sorted_posts.update({post: post_value})
     sorted_posts = dict(sorted(sorted_posts.items(),key=lambda x:x[1],reverse=True))
-    with open(parse_config_json.header_markdown) as header_md, open(header_html) as header: # starting index_html
+    with open(parse_json_config(config, "header_markdown")) as header_md, open(header_html) as header: # starting index_html
         header = header.read()
         header_md = header_md.read()
         header = header.replace("[[$CONTENT]]", markdown.markdown(header_md))
@@ -128,12 +128,14 @@ def read_markdown_create_indices(posts):
             index_html_output = index_html_output.replace("[[$FILE_TAGS]]", str(posts.get(post)[6]))
             index_html_output = index_html_output.replace("[[$FILE_DATE]]", posts.get(post)[2])
             index_html_output = index_html_output.replace("[[$FILE_SUMMARY]]", posts.get(post)[5])
-    with open(parse_config_json.footer_markdown) as footer_md, open(footer_html) as footer: # finishing index_html_output
+    with open(parse_json_config(config, "footer_markdown")) as footer_md, open(footer_html) as footer: # finishing index_html_output
         footer_md = footer_md.read()
         footer = footer.read()
         footer = footer.replace("[[$CONTENT]]", markdown.markdown(footer_md))
         index_html_output = index_html_output + markdown.markdown(footer)
-    with open(parse_config_json.html_directory + "index.html", "w", encoding="utf-8", errors="xmlcharrefreplace") as html_file: # writing index_html_output
+    with open(parse_json_config(config, "html_directory") + "index.html", "w", encoding="utf-8", errors="xmlcharrefreplace") as html_file: # writing index_html_output
         html_file.write(index_html_output)
 
+
 main()
+
--- to_do.md
+++ to_do.md
@@ -3,4 +3,4 @@
 * finish indicies function
 * rewrite date formatting and functionality to include time
 * fix html issues with indices
-
+* rewrite function for creating individual html files, need to change the header rows to be more like the index file
